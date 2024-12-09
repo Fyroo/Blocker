4942fc27ab811d90321e9b669ed843643deaf5c4
Fyroo <chaouimoutaa@gmail.com>
2024-12-09 22:38:48 +0100

    typo

--- src/interface_list_module.py
+++ src/interface_list_module.py
@@ -3,5 +3,5 @@ from scapy.all import *
 def list_interfaces():
     interfaces = []
     for iface_id, iface_data in conf.ifaces.items():
-        interfaces.append(iface_data.name)  # Use 'name' for the interface's human-readable name
+        interfaces.append(iface_data.name) 
     return interfaces
