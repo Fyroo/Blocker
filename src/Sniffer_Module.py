8a38c4c3a71523411b9bd8e69fee0e51ca7cbd57
Fyroo <chaouimoutaa@gmail.com>
2024-12-24 12:56:27 +0100

    added domain ip resolution

--- src/Sniffer_Module.py
+++ src/Sniffer_Module.py
@@ -1,21 +1,21 @@
 from scapy.all import *
 
-
 def procPacket(p, queue):
-    if p.haslayer(IP) and p.haslayer(UDP) and p.haslayer(DNS) and p.haslayer(DNSQR):
-        ip_layer = p.getlayer(IP)
+    if p.haslayer(DNS) and p.haslayer(DNSRR):  
         dns_layer = p.getlayer(DNS)
-        dns_query = dns_layer.qd.qname.decode('utf-8')  
-
-        data = {
-            "domain_ip": ip_layer.dst,  
-            "domain_name": dns_query,  
-        }
-        queue.put(data)  
+        if dns_layer.an:  
+            resolved_ip = dns_layer.an.rdata  
+            dns_query = dns_layer.qd.qname.decode('utf-8') 
+            
+            data = {
+                "domain_ip": resolved_ip,
+                "domain_name": dns_query,
+            }
+            queue.put(data)  
 
 def start_sniffing(queue, stop_event, selected_interface):
     sniff(
-        iface=selected_interface,  
+        iface=selected_interface,
         filter="udp port 53",
         prn=lambda p: procPacket(p, queue),
         stop_filter=lambda x: stop_event.is_set()
